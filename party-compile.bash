f591e8d08fe0fa1e48165763ab0f11de84842910
Hybris95 <hybris_95@hotmail.com>
2011-04-13 16:28:07 +0100

    Forgot to fix compile script on Linux

--- party-compile.bash
+++ party-compile.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac -cp "./class;./jars/craftbukkit.jar;./jars/Permissions.jar" -d "./class" "./src/com/hybris/bukkit/party/*.java"
+javac -Xlint -cp "./class;./jars/craftbukkit.jar;./jars/Permissions.jar" -d "./class" "./src/com/hybris/bukkit/party/*.java"
 
